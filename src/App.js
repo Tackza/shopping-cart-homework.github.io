9abbb4d0b4b8b4cc4274273a3e6a75b8e8644f2f
Pongsatorn <tackpongsatorn@gmail.com>
2020-08-25 23:44:12 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import Cart from './component/Cart';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 function App() {
   const [cart, setCart] = useState([])
+  const [amountTotal , setAmountTotal] = useState(0)
   const [value, setValue] = useState(0)
-  const [notebook] = useState(
+  const [notebook, setNotebook] = useState(
     [
       {
         id: 1,
@@ -23,15 +25,87 @@ function App() {
 
       }, {
         id: 3,
-        image: "https://www.lenovo.com/medias/37-lenovo-thinkbook-15-hero.png?context=bWFzdGVyfHJvb3R8OTM5ODl8aW1hZ2UvcG5nfGg1NC9oNTIvMTA2NzQ5NDE3ODgxOTAucG5nfGM0NDY0YjA2OTE3YmM2ZDljMGZlNDFhZjBiOWFkMTZmZDZjOTkwZjUzNTgyMjIzNDRmMmFiOTk4MjM4YmJhYTI",
+        image: "https://cf.shopee.co.th/file/1721fdf1a127540e94a7f4f36d660440",
         price: 23000,
         name: "HP",
+        amount: 1,
+
+      }
+      , {
+        id: 4,
+        image: "https://cf.shopee.co.th/file/dee416ef9d48c04bc02313f7efdc8f65",
+        price: 29900,
+        name: "Asus",
+        amount: 1,
+
+      }
+      , {
+        id: 5,
+        image: "https://cf.shopee.co.th/file/98ccb8a84db0db24141a4396fa2a59ed",
+        price: 29900,
+        name: "Msi",
+        amount: 1,
+
+      }
+      , {
+        id: 6,
+        image: "https://cf.shopee.co.th/file/89dffaea332d8a6713fb76fc431f5aa2",
+        price: 22990,
+        name: "Acer",
         amount: 1,
 
       }
     ]
 
   )
+  const [first] = useState([
+    {
+      id: 1,
+      image: "https://cf.shopee.co.th/file/e273ae9bb461a8d4469c86921e9b52ec",
+      price: 20000,
+      name: "Macbook",
+      amount: 1,
+
+    }, {
+      id: 2,
+      image: "https://cf.shopee.co.th/file/cef1425da945b2b0128ddebed6c53a38",
+      price: 22000,
+      name: "Asus",
+      amount: 1,
+
+    }, {
+      id: 3,
+      image: "https://cf.shopee.co.th/file/1721fdf1a127540e94a7f4f36d660440",
+      price: 23000,
+      name: "HP",
+      amount: 1,
+
+    }
+    , {
+      id: 4,
+      image: "https://cf.shopee.co.th/file/dee416ef9d48c04bc02313f7efdc8f65",
+      price: 29900,
+      name: "Asus",
+      amount: 1,
+
+    }
+    , {
+      id: 5,
+      image: "https://cf.shopee.co.th/file/98ccb8a84db0db24141a4396fa2a59ed",
+      price: 29900,
+      name: "Msi",
+      amount: 1,
+
+    }
+    , {
+      id: 6,
+      image: "https://cf.shopee.co.th/file/89dffaea332d8a6713fb76fc431f5aa2",
+      price: 22990,
+      name: "Acer",
+      amount: 1,
+
+    }
+  ])
 
 
 
@@ -77,28 +151,89 @@ function App() {
     setValue(result)
   }
 
+  const selectProduct = (value) => {
+    setNotebook(first)
+    const targetProduct = first.filter(item => item.name === value)
+    setNotebook(targetProduct)
+  }
+  const firstAllProduct = () => {
+    setNotebook(first)
+  }
+
+  const amountProduct = () => {
+    const newArray = [...cart]
+    const targetProduct = newArray.map(item => item.amount)
+    const countProduct = targetProduct.reduce((sum , number) => {
+      return sum + number
+    },0)
+    setAmountTotal(countProduct)
+  }
+
 
   return (
-    <div className="App">
-      {notebook.map(item =>
-        <ul>
-          <img src={item.image} style={{ width: "100px" }} />
-          <li>{item.price}</li>
-          <li>{item.name}</li>
-          <button onClick={() => addCart(item.id)}>Add to Cart</button>
-        </ul>
-      )}
-      <Cart
-        addCart={addCart}
-        cart={cart}
-        value={value}
-        plusAmount={plusAmount}
-        minusAmount={minusAmount}
-        deleteItem={deleteItem}
-        checkValue={checkValue}
-      />
+    <div class="container">
+      <div className="row">
+        <div className="col">
+          <nav class="navbar navbar-expand-md navbar-light bg-light">
+            <a class="navbar-brand" type="button" onClick={firstAllProduct}>Tack IT</a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Select Product
+        </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" value="Macbook" onClick={() => selectProduct("Macbook")}>Macbook</a>
+                    <a class="dropdown-item" value="Asus" onClick={() => selectProduct("Asus")}>Asus</a>
+                    <a class="dropdown-item" value="Msi" onClick={() => selectProduct("Msi")}>Msi</a>
+                    <a class="dropdown-item" value="Acer" onClick={() => selectProduct("Acer")}>Acer</a>
+                    <a class="dropdown-item" value="Hp" onClick={() => selectProduct("HP")}>Hp</a>
+                  </div>
+                </li>
 
-    </div>
+              </ul>
+
+            </div>
+          </nav>
+        </div>
+      </div>
+      <br />
+
+
+
+
+      <div className="row">
+        <div className="col-lg-9 col-md-5">
+          <div className="row justify-content-around">
+            {notebook.map(item =>
+              <div className="col-lg-4">
+                <div class="card">
+                  <img src={item.image} class="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title">{item.name}</h5>
+                    <p className="card-text">{Intl.NumberFormat('th-TH', { style: 'currency', currency: 'THB' }).format(item.price)}</p>
+                    <button type="button" class="btn btn-success" onClick={() => addCart(item.id)}>Add to Cart</button>
+                  </div>
+                </div>
+                <br />
+              </div>
+            )}
+          </div>
+        </div>
+        <Cart
+          addCart={addCart}
+          cart={cart}
+          value={value}
+          plusAmount={plusAmount}
+          minusAmount={minusAmount}
+          deleteItem={deleteItem}
+          checkValue={checkValue}
+          amountTotal = {amountTotal}
+          amountProduct={amountProduct}
+        />
+
+      </div>
+    </div >
   );
 }
 
--- src/component/Cart.js
+++ src/component/Cart.js
@@ -2,31 +2,31 @@ import React from 'react'
 
 
 function Cart(props) {
-
-
-
     return (
-        <div>
-            <p>test</p>
+        <div className="col">
+            <div class="card">
+                <div class="card-body list-group-item-secondary">
+    <div onClick={props.checkValue()} onChange={props.amountProduct()}>Subtotal ({props.amountTotal} item) : {Intl.NumberFormat('th-TH', { style: 'currency', currency: 'THB' }).format(props.value)}</div>
+                </div>
+            </div>
+
             {props.cart.map(item =>
-                <ul>
-                    <li key={item.id}><img src={item.image} style={{ width: "40px" }} /></li>
-                    <li>{item.name}</li>
-                    <li>{item.price}</li>
-                    <li><button onClick={() => props.minusAmount(item.id)} disabled={item.amount == 1 ? true : null}>-</button></li>
-                    <li>{item.amount}</li>
-                    <li><button onClick={() => props.plusAmount(item.id)}>+</button></li>
-                    <li><button onClick={() => props.deleteItem(item.id)}>Delete</button></li>
+                <ul class="list-group list-inline">
+                    <li class="list-inline-item ">
+                        <img src={item.image} class="card-img w-50" />
+                        <h5 class="card-title list-inline-item">{item.name}</h5>
+                        <p class="card-text ">price : {Intl.NumberFormat('th-TH', { style: 'currency', currency: 'THB' }).format(item.price)}</p>
+                        <button type="button" class="btn btn-sm btn-outline-info list-inline-item" onClick={() => props.minusAmount(item.id)} disabled={item.amount == 1 ? true : null}>-</button>
+                        <p class="card-text list-inline-item">Qty : {item.amount}</p>
+                        <button type="button" class="btn btn-sm btn-outline-info list-inline-item" onClick={() => props.plusAmount(item.id)}>+</button>
+                        <br />
+                        <button type="button" class="btn btn-sm btn-danger" onClick={() => props.deleteItem(item.id)}>Delete</button>
+                    </li>
+                    <br/>
+                    <br/>
                 </ul>
-            )}
-            <div onClick={props.checkValue()}>{props.value}</div>
-            
-            {/* {props.cart.forEach(item => {
-                            value += (item.price * item.amount)
-                        })} */}
-
-
-
+            )
+            }
         </div>
     )
 }
